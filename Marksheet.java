c8c6fb14ed43c5d054fc9627a1c30e1d5bc8a498
Bhavesh Sharma <bhavesh.sharma@metacube.com>
2020-01-17 11:04:54 +0530

    :Assignment3

--- Marksheet.java
+++ Marksheet.java
@@ -3,6 +3,12 @@ package Assignment3;
 import java.util.*;
 
 class Marksheet {
+	/**
+	 * 
+	 * @param n number of students
+	 * @param st array of students grades
+	 * @return average marks
+	 */
 	double findAvg(int n, int[] st) {
 		double sum = 0;
 		for (int i = 0; i < n; i++) {
@@ -10,7 +16,12 @@ class Marksheet {
 		}
 		return sum / n;
 	}
-
+	/**
+	 * 
+	 * @param n number of students
+	 * @param st array of students grades
+	 * @return maximum marks
+	 */
 	int findMax(int n, int[] st) {
 		int max = 0;
 		for (int i = 0; i < n; i++) {
@@ -20,7 +31,12 @@ class Marksheet {
 		}
 		return max;
 	}
-
+	/**
+	 * 
+	 * @param n number of students
+	 * @param st array of students grades
+	 * @return minimum marks
+	 */
 	int findMin(int n, int[] st) {
 		int min = 100;
 		for (int i = 0; i < n; i++) {
@@ -30,7 +46,12 @@ class Marksheet {
 		}
 		return min;
 	}
-
+	/**
+	 * 
+	 * @param n number of students
+	 * @param st array of students grades
+	 * @return percentage of student passed
+	 */
 	double findPassPercent(int n, int[] st) {
 		double count = 0;
 		for (int i = 0; i < n; i++) {
--- StringOp.java
+++ StringOp.java
@@ -3,10 +3,16 @@ package Assignment3;
 import java.util.*;
 
 class StringOp {
-	static boolean stringComparison(String s1, String s2) {
-		if (s1.length() == s2.length()) {
-			for (int i = 0; i < s1.length(); i++) {
-				if (s1.charAt(i) == s2.charAt(i))
+	/**
+	 * 
+	 * @param str1 input string 1
+	 * @param str2 input string 2
+	 * @return boolean if equals true else false
+	 */
+	static boolean stringComparison(String str1, String str2) {
+		if (str1.length() == str2.length()) {
+			for (int i = 0; i < str1.length(); i++) {
+				if (str1.charAt(i) == str2.charAt(i))
 					continue;
 				else
 					return false;
@@ -16,19 +22,27 @@ class StringOp {
 			return false;
 		}
 	}
-
-	static String stringReverse(String s1) {
-		char[] c2 = new char[s1.length()];
-		for (int i = 0; i < s1.length(); i++) {
-			c2[i] = s1.charAt(s1.length() - 1 - i);
+	/**
+	 * 
+	 * @param str1 str1 input string 1
+	 * @return reverse string
+	 */
+	static String stringReverse(String str1) {
+		char[] c2 = new char[str1.length()];
+		for (int i = 0; i < str1.length(); i++) {
+			c2[i] = str1.charAt(str1.length() - 1 - i);
 		}
 		return new String(c2);
 	}
-
-	static String reverseCase(String s1) {
-		char[] c2 = new char[s1.length()];
-		for (int i = 0; i < s1.length(); i++) {
-			c2[i] = s1.charAt(s1.length() - 1 - i);
+	/**
+	 * 
+	 * @param str1 str1 input string 1
+	 * @return string with change case
+	 */
+	static String reverseCase(String str1) {
+		char[] c2 = new char[str1.length()];
+		for (int i = 0; i < str1.length(); i++) {
+			c2[i] = str1.charAt(str1.length() - 1 - i);
 			if (65 <= c2[i] && c2[i] <= 91) {
 				c2[i] = (char) ((int) c2[i] + 32);
 			} else if (97 <= c2[i] && c2[i] <= 122) {
@@ -37,14 +51,18 @@ class StringOp {
 		}
 		return new String(c2);
 	}
-
-	static String stringLongest(String s1) {
+/**
+ * 
+ * @param str1 str1 input string 1
+ * @return longest word in string
+ */
+	static String stringLongest(String str1) {
 		int i;
 		int j = 1;
-		int[] a = new int[s1.length()];
+		int[] a = new int[str1.length()];
 		a[0] = 0;
-		for (i = 0; i < s1.length(); i++) {
-			if (s1.charAt(i) == ' ' || s1.charAt(i) == s1.length() - 1) {
+		for (i = 0; i < str1.length(); i++) {
+			if (str1.charAt(i) == ' ' || str1.charAt(i) == str1.length() - 1) {
 				a[j] = i;
 				j++;
 			}
@@ -56,12 +74,12 @@ class StringOp {
 				d = a[i] - a[i - 1];
 				index = a[i];
 			}
-			if ((a[i] - a[i - 1]) >= d && a[i] == s1.length() - 1) {
+			if ((a[i] - a[i - 1]) >= d && a[i] == str1.length() - 1) {
 				d = a[i] - a[i - 1] + 1;
 				index = a[i - 1];
 			}
 		}
-		char[] c = s1.toCharArray();
+		char[] c = str1.toCharArray();
 		return new String(c, index, d + 1);
 	}
 
