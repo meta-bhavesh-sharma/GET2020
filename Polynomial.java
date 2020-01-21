33d0ee1c74b0b1345b9c7a04e6f95364bca763e6
meta-bhavesh-sharma <59636741+meta-bhavesh-sharma@users.noreply.github.com>
2020-01-22 01:24:27 +0530

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -10,6 +10,9 @@ public class Polynomial {
 			term.add(new Term(power[i], coofitient[i]));
 		}
 	}
+	Polynomial(ArrayList<Term> list) {
+		term=list;
+	}
 
 	float evaluate(float x) {
 		float sum = 0;
@@ -30,6 +33,7 @@ public class Polynomial {
 	}
 	Polynomial addPoly(Polynomial p1,Polynomial p2)
 	{
+		ArrayList<Term> list=new ArrayList<Term>();
 		Polynomial p3=new Polynomial();
 		for(int i=0;i<p1.term.size();i++)
 		{
@@ -37,7 +41,7 @@ public class Polynomial {
 			{
 				if(p1.term.get(i).power==p2.term.get(j).power)
 				{
-					p3.term.add(new Term(p1.term.get(i).power,p1.term.get(i).coofitient+p2.term.get(j).coofitient));
+					list.add(new Term(p1.term.get(i).power,p1.term.get(i).coofitient+p2.term.get(j).coofitient));
 					p1.term.get(i).coofitient=0;
 					p2.term.get(j).coofitient=0;
 				}	
@@ -46,37 +50,53 @@ public class Polynomial {
 		for(int i=0;i<p1.term.size();i++)
 		{
 			if(p1.term.get(i).coofitient!=0)
-				p3.term.add(new Term(p1.term.get(i).power,p1.term.get(i).coofitient));
+				list.add(new Term(p1.term.get(i).power,p1.term.get(i).coofitient));
 		}
 		for(int i=0;i<p2.term.size();i++)
 		{
 			if(p2.term.get(i).coofitient!=0)
-				p3.term.add(new Term(p2.term.get(i).power,p2.term.get(i).coofitient));
+				list.add(new Term(p2.term.get(i).power,p2.term.get(i).coofitient));
 		}
 
-		return p3;
+		return new Polynomial(list);
 	}
 	Polynomial multiPoly(Polynomial p1,Polynomial p2)
 	{
-		Polynomial p3=new Polynomial();
+		ArrayList<Term> list=new ArrayList<Term>();
 		for(int i=0;i<p1.term.size();i++)
 		{
 			for(int j=0;j<p2.term.size();j++)
 			{
-				p3.term.add(new Term(p1.term.get(i).power+p2.term.get(j).power,p1.term.get(i).coofitient*p2.term.get(j).coofitient));
+				list.add(new Term(p1.term.get(i).power+p2.term.get(j).power,p1.term.get(i).coofitient*p2.term.get(j).coofitient));
 			}
 		}
-		for(int i=0;i<p3.term.size();i++)
+		for(int i=0;i<list.size();i++)
 		{
-			for(int j=i+1;j<p3.term.size();j++)
+			for(int j=i+1;j<list.size();j++)
 			{
-				if(p3.term.get(i).power==p3.term.get(j).power)
+				if(list.get(i).power==list.get(j).power)
 				{
-					p3.term.get(i).coofitient=p3.term.get(i).coofitient+p3.term.get(j).coofitient;
-					p3.term.remove(j);
+					list.get(i).coofitient=list.get(i).coofitient+list.get(j).coofitient;
+					list.remove(j);
 				}
 			}
 		}
-		return p3;
+		return new Polynomial(list);
+	}
+	public static void main(String args[])
+	{
+		int[] p1={0,1,2,3};
+		int[] c1={1,2,3,4};
+		Polynomial x1=new Polynomial(p1,c1);
+		int[] p2={0,1,2,3};
+		int[] c2={1,2,3,4};
+		Polynomial x2=new Polynomial(p2,c2);
+		Polynomial p3=x1.multiPoly(x1,x2);
+		for(int i=0;i<p3.term.size();i++)
+		{
+			System.out.print(p3.term.get(i).power+"  ");
+			System.out.print(p3.term.get(i).coofitient+"  ");
+			System.out.println();
+		}
 	}
 	}
