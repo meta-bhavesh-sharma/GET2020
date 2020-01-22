44a6a2789cc4a27d57409a6f6f402151cb457a68
meta-bhavesh-sharma <59636741+meta-bhavesh-sharma@users.noreply.github.com>
2020-01-23 00:17:57 +0530

    wrong code submitted my mistake

--- Matrices.java
+++ Matrices.java
@@ -1,14 +1,15 @@
 import java.util.*;
 
-
 public class Matrices {
 	ArrayList<Element> list = new ArrayList<Element>();
-	Matrices(int[] e,int[] i,int[] j)
+	int order;
+	Matrices(int[] e,int[] i,int[] j,int n)
 	{
 		for(int k=0;k<e.length;k++)
 		{
 			list.add(new Element(e[k],i[k],j[k]));
 		}
+		order=n;
 	}
 	
 	Matrices transpose()
@@ -23,7 +24,7 @@ public class Matrices {
 			j[k]=list.get(k).column;
 			
 		}
-		return new Matrices(e,i,j);
+		return new Matrices(e,i,j,order);
 		
 	}
 	boolean isSymmetric()
@@ -36,11 +37,63 @@ public class Matrices {
 		}
 		return true;
 	}
-	Matrices add(Matrices m1,Matrices m2)
+	int[][] add(Matrices m1,Matrices m2)
 	{
-		for()
+		int[][] arr = new int[order][order];
+		System.out.println(m1.list.size());
+		for(int k=0;k<m1.list.size();k++)
 		{
-			
+			arr[m1.list.get(k).raw][m1.list.get(k).column]+=m1.list.get(k).element;
+		}
+		for(int k=0;k<m2.list.size();k++)
+		{
+			arr[m2.list.get(k).raw][m2.list.get(k).column]+=m2.list.get(k).element;
+		}
+		return arr;
+	}
+	int[][] multiMatrices(Matrices m1,Matrices m2)
+	{
+		int a[][]=new int[order][order];    
+		int b[][]=new int[order][order];        
+		int c[][]=new int[order][order];
+		for(int k=0;k<m1.list.size();k++)
+		{
+			a[m1.list.get(k).raw][m1.list.get(k).column]+=m1.list.get(k).element;
+		}
+		for(int k=0;k<m2.list.size();k++)
+		{
+			b[m2.list.get(k).raw][m2.list.get(k).column]+=m2.list.get(k).element;
+		}
+		for(int i=0;i<3;i++){    
+		for(int j=0;j<3;j++){    
+		c[i][j]=0;      
+		for(int k=0;k<3;k++)      
+		{      
+		c[i][j]+=a[i][k]*b[k][j];      
+		}  
+		}    
+		}    
+		return c;
+	}
+	public static void main(String args[])
+	{
+		int raw1[]={0,1,2};
+		int column1[]={0,1,2};
+		int element1[]={1,1,1};
+		int raw2[]={2,1,0};
+		int column2[]={0,1,2};
+		int element2[]={1,1,1};
+		int[][] arr=new int[3][3];
+		Matrices m1=new Matrices(element1,raw1,column1,3);
+		Matrices m2=new Matrices(element2,raw2,column2,3);
+		arr=m1.multiMatrices(m1,m2);
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
 		}
 	}
 
