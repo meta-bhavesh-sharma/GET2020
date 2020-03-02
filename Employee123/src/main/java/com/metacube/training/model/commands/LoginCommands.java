1af59fffdd006941f8f6bf485260369ca4dd6702
Bhavesh Sharma <bhavesh.sharma@metacube.com>
2020-03-02 16:18:57 +0530

    :Commit1

---
+++ Employee123/src/main/java/com/metacube/training/model/commands/LoginCommands.java
@@ -0,0 +1,32 @@
+package com.metacube.training.model.commands;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class LoginCommands {
+
+	@NotNull
+	@NotBlank(message = "Field Empty")
+	String email;
+
+	@NotNull
+	@NotBlank(message = "Field Empty")
+	String password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
