3a0c331b65dc3ccb19ba0c3a25d8b34b706ab026
meta-bhavesh-sharma <59636741+meta-bhavesh-sharma@users.noreply.github.com>
2020-04-10 19:32:31 +0530

    final commit

---
+++ GraphEdge/Vertex.java
@@ -0,0 +1,14 @@
+package GraphEdge;
+
+public class Vertex {
+	int startingVertex;
+	int connectedVertex;
+	int distance;
+	public Vertex(int startingVertex,int connectedVertex,int distance)
+	{	
+		this.startingVertex=startingVertex;
+		this.connectedVertex=connectedVertex;
+		this.distance=distance;
+	}
+
+}
